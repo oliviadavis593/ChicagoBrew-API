ca250f09ae4ff2ae7052affd49b56ec24072d792
Olivia Davis <olivia.davis593@gmail.com>
2020-06-19 20:09:37 -0500

    ssl

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,5 +6,6 @@ module.exports = {
     //refers to folder in our app that contains migration steps 
     "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DATABASE_URL
-        : process.env.DATABSE_URL
+        : process.env.DATABSE_URL,
+    "ssl": process.env.DATABSE_URL ? true : false
 }
